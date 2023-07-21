ff56492d6874ef61c19ae728b2495ccfbd0df96f
hsfzxjy <hsfzxjy@gmail.com>
2023-07-21 19:12:02 +0800

    Move Hash to kutil

--- kutil/go.mod
+++ kutil/go.mod
@@ -1,3 +1,7 @@
 module kutil
 
 go 1.20
+
+require go4.org/intern v0.0.0-20230525184215-6c62f75575cb
+
+require go4.org/unsafe/assume-no-moving-gc v0.0.0-20230525183740-e7c30c78aeb2 // indirect
---
+++ kutil/hash.go
@@ -0,0 +1,22 @@
+package ku
+
+import (
+	"unsafe"
+
+	"go4.org/intern"
+)
+
+
+type Hash struct{ v *intern.Value }
+
+func (h Hash) Uint64() uint64 {
+	return uint64(uintptr(unsafe.Pointer(h.v)))
+}
+
+func GetHashForString(str string) Hash {
+	return Hash{intern.GetByString(str)}
+}
+
+func GetHash(obj any) Hash {
+	return Hash{intern.Get(obj)}
+}
