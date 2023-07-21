63a8e7a0c0c42676544d550a094452351ae10a3d
hsfzxjy <hsfzxjy@gmail.com>
2023-07-22 01:53:31 +0800

    Add more Require* functions for testing

---
+++ ktest/require.go
@@ -0,0 +1,21 @@
+package ktest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func RequireAllEqual[T any](t *testing.T, values []T) {
+	for i := 1; i < len(values); i++ {
+		require.Equalf(t, values[0], values[i], "values[0] != values[%d]", i)
+	}
+}
+
+func RequireAllNotEqual[T any](t *testing.T, values []T) {
+	for i := 0; i < len(values); i++ {
+		for j := i + 1; j < len(values); j++ {
+			require.NotEqualf(t, values[i], values[j], "values[%d] == values[%d]", i, j)
+		}
+	}
+}
