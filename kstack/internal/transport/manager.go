92915ccd72c657b2c21cc9d9e2fbcede15cd1506
hsfzxjy <hsfzxjy@gmail.com>
2023-07-25 01:43:11 +0800

    Refactor TransportManager

--- kstack/internal/transport/manager.go
+++ kstack/internal/transport/manager.go
@@ -8,36 +8,40 @@ import (
 	"kstack/internal/tracer"
 	"kstack/internal/transport/slot"
 	ku "kutil"
+	"sync"
 
 	"github.com/puzpuzpuz/xsync/v2"
 )
 
 type _Manager struct {
 	impl internal.Impl
-	m    *xsync.MapOf[ku.Hash, slot.ISlot]
+	m    *xsync.MapOf[ku.Hash, *slot.Slot]
 }
 
 func NewManager(impl internal.Impl) *_Manager {
 	return &_Manager{
 		impl,
-		xsync.NewTypedMapOf[ku.Hash, slot.ISlot](func(s maphash.Seed, k ku.Hash) uint64 {
+		xsync.NewTypedMapOf[ku.Hash, *slot.Slot](func(s maphash.Seed, k ku.Hash) uint64 {
 			return k.Uint64()
 		}),
 	}
 }
 
 func (m *_Manager) slotDisposer(addrHash ku.Hash) ku.F {
+	var once sync.Once
 	return func() {
-		if tracer.Enabled {
-			tracer.T.TrSlotDeleted.Add()
-		}
-		m.m.Delete(addrHash)
+		once.Do(func() {
+			if tracer.Enabled {
+				tracer.T.TrSlotDeleted.Add()
+			}
+			m.m.Delete(addrHash)
+		})
 	}
 }
 
 func (m *_Manager) TrackRemote(itr internal.ITransport) (tr internal.TrackedTransport, err error) {
 	hash := itr.Addr().Hash()
-	m.m.Compute(hash, func(s slot.ISlot, loaded bool) (slot.ISlot, bool) {
+	m.m.Compute(hash, func(s *slot.Slot, loaded bool) (*slot.Slot, bool) {
 		if !loaded {
 			s = slot.New(m.impl, m.slotDisposer(hash))
 		}
@@ -54,35 +58,15 @@ func (m *_Manager) Dial(ctx context.Context, addr internal.IAddr, failFast bool)
 
 	hash := addr.Hash()
 
-	var loadedSlot slot.ISlot
-COMPUTE:
-	select {
-	case <-ctx.Done():
-		if loadedSlot != nil {
-			loadedSlot.TryDispose()
-		}
-		return nil, ctx.Err()
-	default:
-	}
 	var awaiter ku.Awaiter[internal.TrackedTransport]
-	var dialedByThisCall = false
-	m.m.Compute(hash, func(s slot.ISlot, loaded bool) (slot.ISlot, bool) {
+	m.m.Compute(hash, func(s *slot.Slot, loaded bool) (*slot.Slot, bool) {
 		if !loaded {
 			s = slot.New(m.impl, m.slotDisposer(hash))
 		}
-		loadedSlot = s
 		awaiter = s.DialAndTrack(func() (internal.ITransport, error) {
-			dialedByThisCall = true
 			return dialer.DialAddr(ctx, addr)
 		}, failFast)
 		return s, false
 	})
-	tr, err = awaiter()
-	if err == nil {
-		return tr, nil
-	}
-	if dialedByThisCall {
-		return nil, err
-	}
-	goto COMPUTE
+	return awaiter()
 }
